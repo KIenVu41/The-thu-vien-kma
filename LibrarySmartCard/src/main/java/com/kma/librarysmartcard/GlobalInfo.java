f66435b6c2c4df539169d3664aac9f48d851ce6b
KIenVu41 <kienvutrung20@gmail.com>
2022-06-30 21:04:24 +0700

    up code

---
+++ LibrarySmartCard/src/main/java/com/kma/librarysmartcard/GlobalInfo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kma.librarysmartcard;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ *
+ * @author admin
+ */
+public class GlobalInfo {
+    private APDUGenerator card;
+
+    public GlobalInfo(APDUGenerator card) {
+        this.card = card;
+    }
+    
+    public String getID() {
+        String studentInfo = card.getInfo();
+        byte[] bytes = card.hexStringToByteArray(studentInfo);
+        String dataShow = new String(bytes , StandardCharsets.UTF_8);
+        String[] result = dataShow.split("#");
+     
+        return result[0];
+       
+    }
+
+}
